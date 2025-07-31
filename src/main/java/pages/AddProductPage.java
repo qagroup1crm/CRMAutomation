ce2958a706ce525a08d7a8222c7fef81ee3c5530
EmeaTester <emeatest2023@gmail.com>
2025-07-31 14:22:11 -0400

    Updated AddProductPage and AddProduct test for new validation flow

--- src/main/java/pages/AddProductPage.java
+++ src/main/java/pages/AddProductPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AddProductPage extends Base{
@@ -9,6 +11,11 @@ public class AddProductPage extends Base{
 		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(xpath ="//*[contains(@class,'nav-link') and contains(text(),'Products')]")
+	public WebElement Products;		
+			
+	@FindBy(xpath = "//*[@id=\"content\"]/div[2]/div[1]/div/div[1]/div/div[2]/button/span")
+	public WebElement AddProduct;
+	//
 	
-
 }
--- src/test/java/testScipts/AddProduct.java
+++ src/test/java/testScipts/AddProduct.java
@@ -1,12 +1,36 @@
 package testScipts;
 
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 import listeners.CRMListeners;
+import pages.AddProductPage;
+import pages.Login;
 
 @Listeners(CRMListeners.class)
 public class AddProduct extends BaseTest{
 	
+	@BeforeMethod
+	public void navigateToHome() throws IOException {
+		WebDriver driver = BaseTest.getDriver();
+		Login lp = new Login(driver);
+		lp.loginToApp(driver);
+		
+	}
 	
-
+@Test
+public void products()
+{
+	
+	WebDriver driver = BaseTest.getDriver();	
+	AddProductPage pr= new AddProductPage(driver);
+	
+utils.UtilCommon.waitForElementvisible(driver, pr.Products);
+pr.Products.click();
+BaseTest.getTest().info("Product Tab is opened");
+}
 }
